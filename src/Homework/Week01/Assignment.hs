85553787ad68461c8dcf4676784cd91fc06ea1bb
Evan Zacks <evan.zacks@stitchfix.com>
2022-05-02 23:42:26 -0700

    week01: cc numbers
    It seems like making doubleEveryOther work left to right would be more
    straightforward, then we could make use of toDigitsRev in the caller to
    doubleEveryOther.
    
    Right now toDigitsRev is unused other than being the
    maybe-more-efficient helper of toDigits.

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -2,23 +2,34 @@ module Homework.Week01.Assignment where
 
 -- #1a
 toDigits :: Integer -> [Integer]
-toDigits = undefined
+toDigits n = reverse (toDigitsRev n)
+--  | n < 10    = [n]
+--  | otherwise = toDigits (n `div` 10) ++ [(n `mod` 10)]
 
 -- #1b
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = undefined
+toDigitsRev n
+  | n <= 0    = []
+  | n < 10    = [n]
+  | otherwise = (n `mod` 10) : toDigitsRev (n `div` 10)
 
 -- #2
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther n  = reverse (doubleEveryOther' (reverse n))
+
+doubleEveryOther' :: [Integer] -> [Integer]
+doubleEveryOther' []       = []
+doubleEveryOther' (x:[])   = [x]
+doubleEveryOther' (x:y:zs) = [x, y*2] ++ doubleEveryOther' zs
 
 -- #3
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+sumDigits []     = 0
+sumDigits (x:xs) = sum (toDigits x) + sumDigits xs
 
 -- #4
 validate :: Integer -> Bool
-validate = undefined
+validate n = sumDigits (doubleEveryOther (toDigits n)) `mod` 10 == 0
 
 -- #5
 type Peg = String
