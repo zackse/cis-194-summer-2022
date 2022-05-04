313401ddc548d11529ac07a8ee80df57366a559f
Evan Zacks <evan.zacks@stitchfix.com>
2022-05-04 09:56:19 -0700

    hanoi solution

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -40,7 +40,11 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi = undefined
+hanoi 0 a b c = []
+hanoi 1 a b c = [(a,b)]
+hanoi n a b c = hanoi (n-1) a c b ++ [(a,b)] ++ hanoi (n-1) c b a
+--hanoi 2 a b c = hanoi 1 a c b ++ [(a,b)] ++ hanoi 1 c b a
+--hanoi 2 a b c = [(a,c),(a,b),(c,b)]
 
 hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
 hanoi4 = undefined
