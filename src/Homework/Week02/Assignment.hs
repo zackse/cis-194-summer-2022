6d4d78c2f247151b76c23b688f19dcbb599da47d
Evan Zacks <evan.zacks@stitchfix.com>
2022-05-15 16:49:56 -0700

    week02 parseMessage/parse

--- src/Homework/Week02/Assignment.hs
+++ src/Homework/Week02/Assignment.hs
@@ -15,11 +15,23 @@ import Homework.Week02.Log
 
 -- #1a
 parseMessage :: String -> LogMessage
-parseMessage = undefined
+parseMessage = (parseWords . words)
+
+parseWords :: [String] -> LogMessage
+parseWords [] = Unknown []
+parseWords ("I" : t : xs)     = LogMessage Info               (read t) (unwords xs)
+parseWords ("W" : t : xs)     = LogMessage Warning            (read t) (unwords xs)
+parseWords ("E" : e : t : xs) = LogMessage ((Error . read) e) (read t) (unwords xs)
+parseWords s                  = (Unknown . unwords) s
 
 -- #1b
 parse :: String -> [LogMessage]
-parse = undefined
+parse s = map parseMessage $ lines s
+
+--parse = parseLines . lines
+--parseLines :: [String] -> [LogMessage]
+--parseLines []     = []
+--parseLines (x:xs) = parseMessage x : parseLines xs
 
 -- #2
 insert :: LogMessage -> MessageTree -> MessageTree
