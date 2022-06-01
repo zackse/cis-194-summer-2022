5e7497a5247f0e589566ce1901a73baaea09c7d0
Evan Zacks <evan.zacks@stitchfix.com>
2022-06-01 16:10:07 -0700

    week03, except histogram

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -6,11 +6,27 @@ module Homework.Week03.Assignment (
 
 -- #1
 skips :: [a] -> [[a]]
-skips = undefined
+skips [] = []
+skips xs = map (\i -> everyNth i xs) [1..length xs]
+
+-- TODO: use Control.Lens to avoid partial '!!' function
+-- https://stackoverflow.com/a/23627631
+everyNth :: Int -> [a] -> [a]
+everyNth n xs = [xs !! (i-1) | i <- [1..length xs],
+                               i `mod` n == 0]
+
+nth :: Int -> [a] -> Maybe a
+nth n xs = myhead $ take 1 (drop (n-1) xs)
+
+myhead :: [a] -> Maybe a
+myhead [] = Nothing
+myhead (x:_) = Just x
 
 -- #2
 localMaxima :: [Integer] -> [Integer]
-localMaxima = undefined
+localMaxima [] = []
+localMaxima (x:y:z:zs) | x < y && y > z = [y] ++ localMaxima ([y,z] ++ zs)
+localMaxima (x:xs) = localMaxima xs
 
 -- #3
 histogram :: [Integer] -> String
