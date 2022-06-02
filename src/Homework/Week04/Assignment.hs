e858a2c5e98792fe6a0c686f31785fe4fa868ad7
Evan Zacks <evan.zacks@stitchfix.com>
2022-06-01 21:40:00 -0700

    week04 WIP

--- src/Homework/Week04/Assignment.hs
+++ src/Homework/Week04/Assignment.hs
@@ -21,49 +21,57 @@ module Homework.Week04.Assignment (
 
 import Homework.Week04.BST
 
+import Data.Char
+import Data.Maybe
+
 -- #1
 ex1 :: a -> b -> b
-ex1 = undefined
+ex1 _ b = b
 
 -- #2
 ex2 :: a -> a -> a
-ex2 = undefined
+ex2 a _ = a
 
 -- #3
 ex3 :: Int -> a -> a
-ex3 = undefined
+ex3 _ a = a
 
 -- #4
 ex4 :: Bool -> a -> a -> a
-ex4 = undefined
+ex4 _ a _ = a 
 
 -- #5
 ex5 :: Bool -> Bool
-ex5 = undefined
+ex5 True = False
+ex5 _ = True
 
 -- #6
+-- can't be defined?
 ex6 :: (a -> a) -> a
 ex6 = undefined
 
 -- #7
 ex7 :: (a -> a) -> a -> a
-ex7 = undefined
+ex7 f a = f a
 
 -- #8
 ex8 :: [a] -> [a]
-ex8 = undefined
+ex8 [] = []
+ex8 (x:xs) = x:[]
 
 -- #9
 ex9 :: (a -> b) -> [a] -> [b]
-ex9 = undefined
+ex9 f [] = []
+ex9 f (x:xs) = f x : ex9 f xs
 
 -- #10
+-- can't be defined?
 ex10 :: Maybe a -> a
 ex10 = undefined
 
 -- #11
 ex11 :: a -> Maybe a
-ex11 = undefined
+ex11 a = Just a
 
 -- #12
 ex12 :: Maybe a -> Maybe a
@@ -71,15 +79,39 @@ ex12 = undefined
 
 -- #13
 insertBST :: (a -> a -> Ordering) -> a -> BST a -> BST a
-insertBST = undefined
+insertBST _ new Leaf = Node Leaf new Leaf
+insertBST f new (Node left old right)
+  | o == LT = Node (insertBST f new left) old right
+  | o == EQ = Node (insertBST f new left) old right
+  | o == GT = Node left old (insertBST f new right)
+  where o = f new old
+
+safeHead :: [a] -> Maybe a
+safeHead []    = Nothing
+safeHead (x:_) = Just x
+
+safeTail :: [a] -> Maybe [a]
+safeTail []     = Nothing
+safeTail (_:xs) = Just xs
 
 -- #14
 allCaps :: [String] -> Bool
-allCaps = undefined
+allCaps [] = True
+allCaps ("":_) = False
+allCaps (x:xs) = isUpper (fromJust (safeHead x)) && allCaps xs
+
+allCaps' :: [String] -> Bool
+allCaps' xs = xs == filter (\a -> isUpper (fromMaybe 'a' (safeHead a))) xs
 
 -- #15
 dropTrailingWhitespace :: String -> String
-dropTrailingWhitespace = undefined
+dropTrailingWhitespace s = reverse . dropLeadingWhitespace $ reverse s
+
+dropLeadingWhitespace :: String -> String
+dropLeadingWhitespace [] = []
+dropLeadingWhitespace (x:xs)
+  | isSpace x = dropLeadingWhitespace xs 
+  | otherwise = x:xs 
 
 -- #16
 firstLetters :: [String] -> [Char]
--- src/Homework/Week04/BST.hs
+++ src/Homework/Week04/BST.hs
@@ -1,6 +1,6 @@
 module Homework.Week04.BST where
 
-import Test.QuickCheck
+--import Test.QuickCheck
 import Control.Applicative
 
 data BST a = Leaf
@@ -43,22 +43,22 @@ getElements (Node left x right) = getElements left ++ x : getElements right
 -- TESTING CODE. (Students aren't expected to understand this yet, but it
 -- might be interesting to read, anyway!)
 
-instance Arbitrary a => Arbitrary (BST a) where
-  arbitrary = sized mk_tree
-
-mk_tree :: Arbitrary a => Int -> Gen (BST a)
-mk_tree 0 = return Leaf
-mk_tree n = frequency [ (1, return Leaf)
-                      , (2, Node <$> mk_tree (n `div` 2)
-                                 <*> arbitrary
-                                 <*> mk_tree (n `div` 2)) ]
-
-prop_ordered :: BST Int -> Bool
-prop_ordered x = isBST compare x == is_sorted (getElements x)
-  where
-    is_sorted []             = True
-    is_sorted [_]            = True
-    is_sorted (x1 : x2 : xs) = x1 <= x2 && is_sorted (x2 : xs)
-
-test :: IO ()
-test = quickCheck prop_ordered
+-- instance Arbitrary a => Arbitrary (BST a) where
+--   arbitrary = sized mk_tree
+-- 
+-- mk_tree :: Arbitrary a => Int -> Gen (BST a)
+-- mk_tree 0 = return Leaf
+-- mk_tree n = frequency [ (1, return Leaf)
+--                       , (2, Node <$> mk_tree (n `div` 2)
+--                                  <*> arbitrary
+--                                  <*> mk_tree (n `div` 2)) ]
+-- 
+-- prop_ordered :: BST Int -> Bool
+-- prop_ordered x = isBST compare x == is_sorted (getElements x)
+--   where
+--     is_sorted []             = True
+--     is_sorted [_]            = True
+--     is_sorted (x1 : x2 : xs) = x1 <= x2 && is_sorted (x2 : xs)
+-- 
+-- test :: IO ()
+-- test = quickCheck prop_ordered
