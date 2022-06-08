d58c9aeeff5f940dda2476ac733ecacaf807dae1
Evan Zacks <evan.zacks@stitchfix.com>
2022-06-07 23:23:42 -0700

    week05 WIP

--- src/Homework/Week05/Assignment.hs
+++ src/Homework/Week05/Assignment.hs
@@ -27,13 +27,31 @@ evalStr x
   where s = parseExp Lit Add Mul x
 
 -- #3
--- class Expr a where
---   lit :: ???
---   add :: ???
---   mul :: ???
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+  reify :: a -> a
+  reify = id
 
 -- #4
--- instance Expr Integer where
---   lit = ???
---   add = ???
---   mul = ???
+instance Expr Integer where
+  lit a = a
+  add a b = a + b
+  mul a b = a * b
+
+instance Expr Bool where
+  lit a = a > 0
+  add a b = a || b
+  mul a b = a && b
+
+newtype MinMax  = MinMax Integer deriving (Eq, Show)
+newtype Mod7    = Mod7 Integer deriving (Eq, Show)
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool    = testExp :: Maybe Bool
+-- testMM      = testExp :: Maybe MinMax
+-- testSat     = testExp :: Maybe Mod7
